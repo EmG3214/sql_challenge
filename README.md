79fec5093ff2c2172a0b9860293fa4995c657b46
Em Greene <EmGreene3214@gmail.com>
2023-04-18 22:10:45 -0400

    adding files, ERD, and first code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sql_challenge
 sql coding challenge
+
+see the ERD.sql for the coded ERD, there is also an image attached in this file folder called ERDimage.
+
+All tables have primary keys, but for dept_emp and dept_manager, we use a composite key.
+
+I used pgAdmin 4 to import the data into the tables.
---
+++ main_code.sql
@@ -0,0 +1,47 @@
+CREATE TABLE departments (
+    dept_no VARCHAR(10) NOT NULL,
+    dept_name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(10) NOT NULL,
+    emp_no INTEGER NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE employees (
+emp_no INTEGER NOT NULL,
+emp_title_id VARCHAR(10) NOT NULL,
+birth_date DATE NOT NULL,
+first_name VARCHAR(40) NOT NULL,
+last_name VARCHAR(40) NOT NULL,
+sex VARCHAR(4) NOT NULL,
+hire_date DATE NOT NULL,
+PRIMARY KEY (emp_no),
+FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+
+CREATE TABLE salaries (
+emp_no INTEGER NOT NULL,
+salary INTEGER NOT NULL,
+PRIMARY KEY (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(10) NOT NULL,
+    title VARCHAR(40) NOT NULL,
+    PRIMARY KEY (title_id)
+);
+
